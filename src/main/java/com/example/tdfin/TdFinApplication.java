793f6b75cab11840ac1b87d363a2825b4fbad231
Karen Avdiev <k.avdiev@degroofpetercam.com>
2021-07-02 14:06:33 +0200

    prj creation

---
+++ src/main/java/com/example/tdfin/TdFinApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdFinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdFinApplication.class, args);
+    }
+
+}
